eec13de601f296569732b0a8d7e3971202dc16bc
Coulson_Wang <wangyuanyi1993@126.com>
2017-08-05 12:34:05 +0800

    调整项目文件结构

---
+++ CWCalendarLabel/CWCalendarLabel/CWCalendarLabelDemo/Classes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CWCalendarLabel
+//
+//  Created by Coulson_Wang on 2017/8/5.
+//  Copyright © 2017年 Coulson_Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CWCalendarLabel/CWCalendarLabelTests/CWCalendarLabelTests.m
@@ -0,0 +1,39 @@
+//
+//  CWCalendarLabelTests.m
+//  CWCalendarLabelTests
+//
+//  Created by Coulson_Wang on 2017/8/5.
+//  Copyright © 2017年 Coulson_Wang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CWCalendarLabelTests : XCTestCase
+
+@end
+
+@implementation CWCalendarLabelTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
