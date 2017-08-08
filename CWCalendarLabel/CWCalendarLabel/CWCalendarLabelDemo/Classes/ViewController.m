b0ac94b8d5220dea967ca7244f449f15faa2c8a5
Coulson_Wang <wangyuanyi1993@126.com>
2017-08-08 17:23:01 +0800

    增加一个自定义属性，确定是否自适应动画文本的大小

--- CWCalendarLabel/CWCalendarLabel/CWCalendarLabelDemo/Classes/ViewController.m
+++ CWCalendarLabel/CWCalendarLabel/CWCalendarLabelDemo/Classes/ViewController.m
@@ -22,12 +22,13 @@
     
     CWCalendarLabel *calendarLabel = [[CWCalendarLabel alloc] init];
     calendarLabel.text = @"99";
-    calendarLabel.textAlignment = NSTextAlignmentCenter;
-    calendarLabel.bounds = CGRectMake(0, 0, 100, 30);
+    calendarLabel.textAlignment = NSTextAlignmentRight;
+    calendarLabel.bounds = CGRectMake(0, 0, 150, 60);
     calendarLabel.center = self.view.center;
     calendarLabel.font = [UIFont systemFontOfSize:30];
     calendarLabel.animateDuration = 0.5;
     calendarLabel.enableWhenSame = NO;
+    calendarLabel.sizeToFitOn = YES;
     
     [self.view addSubview:calendarLabel];
     
